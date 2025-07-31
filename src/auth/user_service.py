4840045f003b9417e527a6af98256ca638af5be5
4joel2 <mantikjoel@gmail.com>
2025-07-31 21:07:14 +0200

    feat: creating users

---
+++ src/auth/user_service.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Session
+from ..models.models import User 
+from auth.auth import hash_password, verify_password
+
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(User).filter(User.username == username).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+
+def create_user(db: Session, username: str, email: str, password: str):
+    hashed_password = hash_password(password)
+    db_user = User(
+        username=username,
+        email=email,
+        hashed_password=hashed_password
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def authenticate_user(db: Session, username: str, password: str):
+    user = get_user_by_username(db, username)
+    if not user or not verify_password(password, user.hashed_password):
+        return False
+    return user
