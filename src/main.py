1b58435d12288718b6d4979976fec9542790011a
4joel2 <mantikjoel@gmail.com>
2025-07-31 18:57:13 +0200

    feat: basic api initialization

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from auth_router import router as auth_router
+
+app = FastAPI()
+
+app.include_router(auth_router)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
